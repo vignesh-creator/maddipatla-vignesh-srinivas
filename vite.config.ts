ed132471210bcae5f5ae556bbc6b78f1a13895e3
vignesh-creator <vigneshmadhipatla2001@gmail.com>
2025-01-15 11:22:43 +0530

    new edits

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/maddipatla-vignesh-srinivas/',
+  base: '/MaddipatlaVigneshSrinivas.github.io/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
